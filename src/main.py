ede6371009f8f1e3e1885fd4ec1cdcd7e4d946f8
o-x-y-g-e-n <shivamgohel777@gmail.com>
2018-02-24 17:37:14 +0530

    most of them added

--- src/main.py
+++ src/main.py
@@ -16,7 +16,12 @@ import flirt
 import os
 import xml.etree.ElementTree as ET
 import random
-
+import mytime
+import lyrics
+import definine
+import weather
+import youtubee
+import news
 browser = webdriver.Chrome("/home/oxygen_/Documents/chromedriver")
 
 s = "https://web.whatsapp.com/"
@@ -28,7 +33,7 @@ time.sleep(5)
 browser.find_element_by_xpath('//*[@id="app"]/div/div/div[2]/div/div[3]/label/input').click()
 
 
-time.sleep(10)
+time.sleep(15)
 
 commands = ["/quote","/funfact","/time","/joke","/calc","/conv","/lyrics","/define","/news","/weather","/wikipedia"]
 
@@ -59,29 +64,76 @@ while True:
 	if(system_hr == wht_hr and system_min == wht_min):
 	# print(lst_cmd_text)
 		if(lst_cmd_text == '/quote'):
+			print("quote here\n")
 			ele = browser.find_element_by_xpath('//*[@id="main"]/footer/div[1]/div[2]')
 			tez = quote.quote_generator()
 			ele.send_keys(str(tez))
 			btn = browser.find_element_by_class_name("_2lkdt")
 			btn.click()
 		elif(lst_cmd_text == '/fact'):
+			print("fact here\n")
 			ele = browser.find_element_by_xpath('//*[@id="main"]/footer/div[1]/div[2]')
 			tez = fact.fact_generator()
 			ele.send_keys(str(tez))
 			btn = browser.find_element_by_class_name("_2lkdt")
 			btn.click()
 		elif(lst_cmd_text == '/flirt'):
+			print("flirt here\n")
 			ele = browser.find_element_by_xpath('//*[@id="main"]/footer/div[1]/div[2]')
 			tez = flirt.flirt_generator()
 			ele.send_keys(str(tez))
 			btn = browser.find_element_by_class_name("_2lkdt")
 			btn.click()		
 		elif(lst_cmd_text == '/joke'):
+			print("joke here\n")
 			ele = browser.find_element_by_xpath('//*[@id="main"]/footer/div[1]/div[2]')
 			tez = joke.joke_generator()
 			ele.send_keys(str(tez))
 			btn = browser.find_element_by_class_name("_2lkdt")
 			btn.click()
-		
+		elif(lst_cmd_text == '/time'):
+			print("time here\n")
+			ele = browser.find_element_by_xpath('//*[@id="main"]/footer/div[1]/div[2]')
+			tez = mytime.time_generator()
+			ele.send_keys(str(tez))
+			btn = browser.find_element_by_class_name("_2lkdt")
+			btn.click()
+		elif(str(lst_cmd_text[0:7]) == "/lyrics"):
+			print("lyrics here\n")
+			ele = browser.find_element_by_xpath('//*[@id="main"]/footer/div[1]/div[2]')
+			tez = lyrics.lyrics_generator(str(lst_cmd_text))
+			ele.send_keys(tez)
+			btn = browser.find_element_by_class_name("_2lkdt")
+			btn.click()
+		elif(str(lst_cmd_text[0:7]) == "/define"):
+			print("define here\n")
+			ele = browser.find_element_by_xpath('//*[@id="main"]/footer/div[1]/div[2]')
+			tez = definine.define_generator(str(lst_cmd_text))
+			ele.send_keys(tez)
+			btn = browser.find_element_by_class_name("_2lkdt")
+			btn.click()
+		elif(str(lst_cmd_text[0:8]) == "/weather"):
+			print("weather here\n")
+			ele = browser.find_element_by_xpath('//*[@id="main"]/footer/div[1]/div[2]')
+			tez = weather.weather_generator(str(lst_cmd_text))
+			ele.send_keys(tez)
+			btn = browser.find_element_by_class_name("_2lkdt")
+			btn.click()
+		elif(str(lst_cmd_text[0:8]) == "/youtube"):
+			print("youtbe here\n")
+			ele = browser.find_element_by_xpath('//*[@id="main"]/footer/div[1]/div[2]')
+			tez = youtubee.youtube_generator(str(lst_cmd_text))
+			ele.send_keys(tez)
+			time.sleep(12)
+			btn = browser.find_element_by_class_name("_2lkdt")
+			btn.click()
+		elif(str(lst_cmd_text[0:5]) == '/news'):
+			print("news here\n")
+			ele = browser.find_element_by_xpath('//*[@id="main"]/footer/div[1]/div[2]')
+			tez = news.news_generator(str(lst_cmd_text))
+			for t in tez:
+				ele.send_keys(t)
+				btn = browser.find_element_by_class_name("_2lkdt")
+				btn.click()
 
 time.sleep(1000000)
